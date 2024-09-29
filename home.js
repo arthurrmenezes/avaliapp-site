4682ac02c7f5ad4777390d96de9cfdfb65cf84b4
Arthur Menezes <arthur.menezes2003@hotmail.com>
2024-09-29 13:10:33 -0300

    animacao nos botoes da home

---
+++ home.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('nav ul li a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',  
+                block: 'start'
+            });
+        }
+    });
+});
